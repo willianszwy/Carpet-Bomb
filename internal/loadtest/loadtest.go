74a466f24156f18cc77ff91e073718da3706d135
Willians Oliveira <willianszwy@gmail.com>
2024-04-06 22:06:33 -0400

    wip: run

--- internal/loadtest/loadtest.go
+++ internal/loadtest/loadtest.go
@@ -1,6 +1,12 @@
 package loadtest
 
-import "net/url"
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
 
 type LoadTest struct {
 	url        *url.URL
@@ -17,5 +23,24 @@ func NewTest(url *url.URL, request int, concurrent int) *LoadTest {
 }
 
 func (lt *LoadTest) Run() {
+	startTime := time.Now()
+	ctx := context.Background()
+	defer ctx.Done()
 
+	for i := range lt.req {
+		log.Println(i)
+		doRequest(lt.url)
+	}
+
+	log.Println(time.Since(startTime))
+}
+
+func doRequest(url *url.URL) {
+
+	response, err := http.Get(url.String())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	println(response.StatusCode)
 }
