ae6fbb9f3d0de71df7388480821467d14f4463b6
Willians Oliveira <willianszwy@gmail.com>
2024-04-07 21:20:24 -0400

    feat: results

--- internal/loadtest/loadtest.go
+++ internal/loadtest/loadtest.go
@@ -10,16 +10,31 @@ import (
 )
 
 type LoadTest struct {
-	url        *url.URL
-	req        int
-	concurrent int
+	url            *url.URL
+	req            int
+	concurrent     int
+	ResponseStatus map[int]int
+	mutex          sync.Mutex
 }
 
 func NewTest(url *url.URL, request int, concurrent int) *LoadTest {
+	if concurrent < 1 {
+		concurrent = 1
+	}
+
+	if request < 1 {
+		request = 1
+	}
+
+	if concurrent > request {
+		concurrent = request
+	}
+
 	return &LoadTest{
-		url:        url,
-		req:        request,
-		concurrent: concurrent,
+		url:            url,
+		req:            request,
+		concurrent:     concurrent,
+		ResponseStatus: map[int]int{},
 	}
 }
 
@@ -31,25 +46,39 @@ func (lt *LoadTest) Run() {
 	wg := &sync.WaitGroup{}
 
 	reqByWorker := lt.req / lt.concurrent
+	remainder := lt.req % lt.concurrent
 
 	for i := range lt.concurrent {
 		wg.Add(1)
 		println("add worker: ", i)
 		go func() {
 			defer wg.Done()
-			worker(i, lt.url, reqByWorker)
+			tr := reqByWorker
+			if i == (lt.concurrent - 1) {
+				tr += remainder
+			}
+			worker(i, lt.url, tr, lt.salveResponses)
 		}()
 	}
 
 	wg.Wait()
 
-	log.Println(time.Since(startTime))
+	println("Results:")
+	println("Total time:", time.Since(startTime).String())
+	println("Total requests:", lt.req)
+	for key, value := range lt.ResponseStatus {
+		println("status:", key, "total:", value)
+	}
+
 }
 
-func worker(id int, url *url.URL, totalReq int) {
+func worker(id int, url *url.URL, totalReq int, counter func(int)) {
+	log.Println("worker:", id, "total request:", totalReq)
 	for i := range totalReq {
 		log.Println("worker:", id, "request:", i)
-		log.Println("response code:", makeRequest(url))
+		statusCode := makeRequest(url)
+		log.Println("response code:", statusCode)
+		counter(statusCode)
 	}
 }
 
@@ -61,4 +90,16 @@ func makeRequest(url *url.URL) int {
 		return response.StatusCode
 	}
 	return response.StatusCode
+}
+
+func (lt *LoadTest) salveResponses(statusCode int) {
+	lt.mutex.Lock()
+	defer lt.mutex.Unlock()
+
+	count, exists := lt.ResponseStatus[statusCode]
+
+	if !exists {
+		count = 0
+	}
+	lt.ResponseStatus[statusCode] = count + 1
 }
